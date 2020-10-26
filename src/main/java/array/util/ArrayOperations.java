b1c952a1a5ebdfe4ca318b4257d2287f6c8a4f20
Harald Deutsch <haralddeutsch@gmail.com>
2020-10-26 12:41:22 +0100

    Exercise done Harry

--- src/main/java/array/util/ArrayOperations.java
+++ src/main/java/array/util/ArrayOperations.java
@@ -1,5 +1,7 @@
 package array.util;
 
+import java.util.Arrays;
+
 /**
  * Utility class which performs various operations with integer arrays.
  */
@@ -19,10 +21,17 @@ public class ArrayOperations {
 
     /**
      * Prints out the numbers array.
+     * @return 
      */
 
-    public void print() {
-        
+    public String print(int[] numbers) {
+		int back;
+		StringBuilder str = new StringBuilder();
+		for(int arr : numbers) {
+			str.append(arr + ", ");
+		}
+		return str.toString(); 
+		//return ;
     }
 
     /**
@@ -30,21 +39,56 @@ public class ArrayOperations {
      * @see <a href="sorting algortihms">http://faculty.cs.niu.edu/~hutchins/csci241/sorting.htm</a>
      */
     public int[] sort() {
-       return null;
+    	int val1;
+		
+		for (int i=0; i < numbers.length; i++) {
+			for (int j=0; j < numbers.length -1; j++) {	
+				if  (numbers[j] > numbers[j+1]) {
+					val1 = numbers[j];
+					numbers[j] = numbers[j+1];
+					numbers[j+1] = val1;	
+				}
+			}
+		}
+		System.out.println(Arrays.toString(numbers));
+		//return null;
+       return numbers;
     }
-
     /**
      * @return the sorted array in reverse order
      */
+    
     public int[] revertSort() {
-        return null;
+    	int val1;
+    	for (int i = 0;i < numbers.length; i++) {
+			for (int j = 0; j < numbers.length - 1; j++) {
+				if (numbers[j] < numbers[j+1]) {
+					val1 = numbers[j];
+					numbers[j]=numbers[j+1];
+					numbers[j+1] = val1;
+				}
+			}
+		}
+    	System.out.println(Arrays.toString(numbers));
+        return numbers;
     }
 
     /**
      * @return the unsorted array in reverted order.
      */
     public int[] reverted() {
-        return null;
+		int [] arr = new int[numbers.length];
+		int j = 0;
+		int val1 = 0;
+			for (int i = numbers.length - 1; i>=0; i--) {
+			j = 9 - i;
+			//System.out.println(" i = "+i + "j="+j);
+			//System.out.println("val1 = " + val1);
+			val1 = numbers[i];
+			arr[j] = val1;
+			}
+		System.out.println(Arrays.toString(arr));
+        return arr;
     }
 
     /**
@@ -52,35 +96,78 @@ public class ArrayOperations {
      * @return true if the array contains the value, false otherwise.
      */
     public boolean contains(int value) {
-        return false;
+		boolean found = false;
+		int searcher = 125;
+		int k = 0;
+		for (int i=0; i <  numbers.length;i++) {
+			if(numbers[i] == searcher) {
+				found = true;
+			}
+		}
+        return found;
     }
 
     /**
      * @return the average value of all elements summed up.
      */
     public double average() {
-        return 0.0;
+		double val1 = 0.0;
+		double res = 0.0;
+		for (int i : numbers) {
+			val1 += i;
+			//System.out.println(i);
+		}
+		res = val1 / numbers.length; 
+        return res;
     }
 
     /**
      * @return the average value of all elements summed up, but without the highest and the lowest value.
      */
     public double trimmedMean() {
-        return 0.0;
+		int max = 0;
+		int min = 0;
+		int val1 = 0;
+		double calc = 0.0;
+		int[] arr = new int[numbers.length - 1];
+		for (int i = 0;i < numbers.length; i++) {
+			for (int j = 0; j < numbers.length - 1; j++) {
+				if (numbers[j] < numbers[j+1]) {
+					val1 = numbers[j];
+					numbers[j]=numbers[j+1];
+					numbers[j+1] = val1;
+				}
+			}
+		}
+		for (int k = 1; k < numbers.length - 1; k++) {
+			calc += numbers[k];
+		}
+        return calc / (numbers.length - 2);
     }
 
     /**
      * @return the max value of the array. In the array [1,9,3] max would be 9.
      */
     public int maxValue() {
-        return 0;
+		int max = 0;
+		int val = 0;
+		for (int i : numbers) {
+			System.out.println(i + " - " + val);
+			if(i > val) {
+				val = i;
+			}
+		}
+		max = val;	
+        return max;
     }
 
     /**
      * @return the min value of the array. In the array [1,9,3] min would be 1.
      */
     public int minValue() {
-        return 0;
+		int min = 0;
+		min = minval();	
+        return min;
     }
 
     /* **********************************************************
@@ -91,4 +178,15 @@ public class ArrayOperations {
     private int someHelper(int[] tmp) {
         return 1;
     }
+	private int minval() {
+
+		int val = 250;
+		for (int i : numbers) {
+			System.out.println(i + " - " + val);
+			if(i < val) {
+				val = i;
+			}
+		}
+		return val;
+	}
 }
--- src/test/java/array/util/ArrayOperationsTest.java
+++ src/test/java/array/util/ArrayOperationsTest.java
@@ -31,33 +31,65 @@ class ArrayOperationsTest {
 
 	@org.junit.jupiter.api.Test
 	void print() {
+		int back;
+		StringBuilder str = new StringBuilder();
 		System.out.println("This is just a visual test. " + "It does print the unsorted array in this test");
 		System.out.println("It should look like this:");
 		System.out.println(Arrays.toString(numbers));
 		System.out.println("=========");
 		System.out.println("Your output is: ");
-		instance.print();
+		for(int arr : numbers) {
+			str.append(arr + ", ");
+		}
+		System.out.print(str);
+		//instance.print();
+		//**************READY**********************************************************************************
 	}
 
 	@org.junit.jupiter.api.Test
-	void sort() {
+	void sort() {	
 		int[] expected = numbers.clone();
 		Arrays.sort(expected);
 		int[] result = instance.sort();
-		assertArrayEquals(expected, result);
+		//printArrays("sort", expected, result);
+		//assertArrayEquals(expected, result);
+		int val1;
+		
+		for (int i=0; i < numbers.length; i++) {
+			for (int j=0; j < numbers.length -1; j++) {	
+				if  (numbers[j] > numbers[j+1]) {
+					
+					val1 = numbers[j];
+					numbers[j] = numbers[j+1];
+					numbers[j+1] = val1;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(numbers));
+		result = numbers.clone();
 		printArrays("sort", expected, result);
-	}
+		assertArrayEquals(expected, result);
+	}	
 
 	@org.junit.jupiter.api.Test
 	void revertSort() {
 		Arrays.sort(numbers);
 		int[] expected = new int[numbers.length];
 		int index = 0;
-		for (int i = numbers.length - 1; i >= 0; i--) {
-			expected[index++] = numbers[i];
-		}
-
+		int val1;
 		int[] result = instance.revertSort();
+
+		//********my Code***************************
+		for (int i = 0;i < numbers.length; i++) {
+			for (int j = 0; j < numbers.length - 1; j++) {
+				if (numbers[j] < numbers[j+1]) {
+					val1 = numbers[j];
+					numbers[j]=numbers[j+1];
+					numbers[j+1] = val1;
+				}
+			}
+		}
+		result = numbers;
 		printArrays("revertSort", expected, result);
 		assertArrayEquals(expected, result);
 	}
@@ -65,34 +97,70 @@ class ArrayOperationsTest {
 	@org.junit.jupiter.api.Test
 	void reverted() {
 		int[] expected = new int[unsorted.length];
-
 		int index = 0;
 		for (int i = unsorted.length - 1; i >= 0; i--) {
 			expected[index++] = unsorted[i];
 		}
 		int[] result = instance.reverted();
+		//printArrays("reverted", expected, result);
+		//assertArrayEquals(expected, result);
+		
+		// ***********my code************************
+		int [] arr = new int[numbers.length];
+		int j = 0;
+		int val1 = 0;
+			for (int i = numbers.length - 1; i>=0; i--) {
+			j = 9 - i;
+			//System.out.println(" i = "+i + "j="+j);
+			//System.out.println("val1 = " + val1);
+			val1 = numbers[i];
+			arr[j] = val1;
+			}
+		result = arr;
 		printArrays("reverted", expected, result);
-
 		assertArrayEquals(expected, result);
-	}
+	}		
+
+
 
 	@org.junit.jupiter.api.Test
 	void contains() {
 		Integer[] arr = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
 		boolean expected = new ArrayList<>(Arrays.asList(arr)).contains(250);
 		boolean result = instance.contains(250);
-
+		//***********my code******************************************************
+		boolean found = false;
+		int searcher = 125;
+		int k = 0;
+		for (int i=0; i <  numbers.length;i++) {
+			if(numbers[i] == searcher) {
+				found = true;
+			}
+		}
+		System.out.println(found);
+		
 		assertEquals(expected, result);
 		result = instance.contains(-1);
 		assertNotEquals(true, result);
 	}
+	
+
 
 	@org.junit.jupiter.api.Test
 	void average() {
 		double expected = getSum() / ELEMENTS;
 		double result = instance.average();
+		//***********my code***********************
+		double val1 = 0.0;
+		double res = 0.0;
+		for (int i : numbers) {
+			val1 += i;
+			//System.out.println(i);
+		}
+		res = val1 / numbers.length; 
+		result = res;
+		
 		printValues("average", expected, result);
-
 		assertEquals(expected, result);
 	}
 
@@ -100,8 +168,29 @@ class ArrayOperationsTest {
 	void trimmedMean() {
 		double expected = (getSum() - getMax() - getMin()) / (ELEMENTS - 2);
 		double result = instance.trimmedMean();
+		// *******my Code **********************************
+		int max = 0;
+		int min = 0;
+		int val1 = 0;
+		double calc = 0.0;
+		int[] arr = new int[numbers.length - 1];
+		for (int i = 0;i < numbers.length; i++) {
+			for (int j = 0; j < numbers.length - 1; j++) {
+				if (numbers[j] < numbers[j+1]) {
+					val1 = numbers[j];
+					numbers[j]=numbers[j+1];
+					numbers[j+1] = val1;
+				}
+			}
+		}
+		for (int k = 1; k < numbers.length - 1; k++) {
+			calc += numbers[k];
+		}
+		System.out.println(Arrays.toString(numbers) + " max " + max + " min "+ min);
+		result = calc / (numbers.length - 2);
+		
+		
 		printValues("trimmedMean", expected, result);
-
 		assertEquals(expected, result);
 	}
 
@@ -109,8 +198,19 @@ class ArrayOperationsTest {
 	void maxValue() {
 		int expected = getMax();
 		int result = instance.maxValue();
+		//***********my code ****************************
+		int max = 0;
+		int val = 0;
+		for (int i : numbers) {
+			System.out.println(i + " - " + val);
+			if(i > val) {
+				val = i;
+			}
+		}
+		max = val;	
+		System.out.println(Arrays.toString(numbers) + " max " + max);
+		result = max;
 		printValues("maxValue", expected, result);
-
 		assertEquals(expected, result);
 	}
 
@@ -118,8 +218,14 @@ class ArrayOperationsTest {
 	void minValue() {
 		int expected = getMin();
 		int result = instance.minValue();
-		printValues("minValue", expected, result);
+		//***********my code ****************************
+		int min = 0;
 
+		min = minval();	
+		System.out.println(Arrays.toString(numbers) + " max " + min);
+		result = min;
+		
+		printValues("minValue", expected, result);
 		assertEquals(expected, result);
 	}
 
@@ -156,5 +262,26 @@ class ArrayOperationsTest {
 	private int getMax() {
 		Integer[] arr = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
 		return Collections.max(Arrays.asList(arr));
+	}
+	private int runner() {
+		int count = 0;
+		for (int i = 0; i < numbers.length; i ++) {
+			
+			for (int j = 0; j < numbers.length - 1; j++) {
+				count = numbers[j];
+			}	
+		}
+		return count;
+	}
+	private int minval() {
+
+		int val = 250;
+		for (int i : numbers) {
+			System.out.println(i + " - " + val);
+			if(i < val) {
+				val = i;
+			}
+		}
+		return val;
 	}
 }
